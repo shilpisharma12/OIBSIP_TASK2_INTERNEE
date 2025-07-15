d2934218d9fcbe8be0324e67680b7b0e0545f097
shilpi sharma <limitlessdesign.in@gmail.com>
2025-07-15 22:29:28 +0530

    Create UFW_Oasis_task2.sh

---
+++ UFW_Oasis_task2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#this is to help other user to complete this easily 
+#This is not for copy and paste this is for knowledge purpose
+sudo ufw allow ssh
+sudo ufw deny http
+sudo ufw enable
+sudo ufw status
